ed96b1cedc491f41273ce3d12f682b8848ee9d08
Chiran <pcnfernando@gmail.com>
2017-10-25 20:19:26 +0530

    Bump Siddhi version and implement abstract method

--- component/src/main/java/org/wso2/extension/siddhi/map/json/sourcemapper/JsonSourceMapper.java
+++ component/src/main/java/org/wso2/extension/siddhi/map/json/sourcemapper/JsonSourceMapper.java
@@ -184,6 +184,11 @@ public class JsonSourceMapper extends SourceMapper {
         }
     }
 
+    @Override
+    protected boolean allowNullInTransportProperties() {
+        return !failOnMissingAttribute;
+    }
+
     /**
      * Convert the given JSON string to {@link Event}.
      *
